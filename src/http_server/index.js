50f08d420715bafe980aedb3a8c2499ea7934401
nikitakonan <nikitakonan1991@gmail.com>
2025-11-16 00:15:14 +0400

    refactor: modernize HTTP server with async/await and ES6 imports

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,17 +1,19 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as http from 'http';
+import { readFile } from 'node:fs/promises';
+import { resolve, dirname } from 'node:path';
+import { createServer } from 'node:http';
 
-export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+export const httpServer = createServer(async function (req, res) {
+  const __dirname = resolve(dirname(''));
+  const file_path =
+    __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+
+  try {
+    const data = await readFile(file_path);
+    res.writeHead(200);
+    res.end(data);
+  } catch (err) {
+    res.writeHead(404);
+    res.end(JSON.stringify(err));
+    return;
+  }
 });
