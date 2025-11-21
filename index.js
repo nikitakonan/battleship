fd8dcbe3c3c37720299cdb5f863188af955049cb
nikitakonan <nikitakonan1991@gmail.com>
2025-11-21 17:54:50 +0400

    feat: start web socket server

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import { httpServer } from "./src/http_server/index.js";
+import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 8181;
+const WS_PORT = 3000;
+
+const wss = new WebSocketServer({
+  port: WS_PORT
+})
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send(JSON.stringify({
+    type: 'handshake',
+    payload: 'Welcome to WebSocket server!'
+  }));
+});
+
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
